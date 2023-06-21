f3013daf7fa283b42248ee39efd9584f1170b260
Zupi <dubravac.neven@gmail.com>
2023-06-22 01:33:59 +0200

    I have finished the JavaScript logic. I need to improve the code, try to shorten it, etc.

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirmPassword');
 const submitBtn = document.querySelector("button");
-const errorMSG = document.querySelector(".errorMSG");
+const errorMessage = document.querySelector(".errorMSG");
 
 
 submitBtn.addEventListener("click", function (event) {
     event.preventDefault();
-    errorMSG.textContent = "";
+    errorMessage.textContent = "";
+    password.classList.remove("invalid");
+    confirmPassword.classList.remove("invalid");
+    password.classList.remove("valid");
+    confirmPassword.classList.remove("valid");
+    errorMessage.classList.remove("showElement");
+
     passwordValue = password.value;
     confirmPasswordValue = confirmPassword.value;
     console.log(passwordValue, confirmPasswordValue);
 
-    if (password.value != confirmPassword.value) {
-        errorMSG.textContent = "Passwords do not match"
-        errorMSG.style.visibility = "visible";
-        password.style.borderColor = "red";
-        confirmPassword.style.borderColor = "red";
-    } 
-    
-    else if (password.value.length < 5) {
-        errorMSG.textContent = "The password length must be 5 or more characters.";
-        errorMSG.style.visibility = "visible";
-        password.style.borderColor = "red";
-        confirmPassword.style.borderColor = "red";
+    if (password.value.length < 5) {
+        errorMessage.textContent = "The password length must be 5 or more characters.";
+        password.classList.add("invalid");
+        confirmPassword.classList.add("invalid");
+        errorMessage.classList.add("showElement");
     }
 
+    else if (password.value != confirmPassword.value) {
+        errorMessage.textContent = "Passwords do not match";
+        password.classList.add("invalid");
+        confirmPassword.classList.add("invalid");
+        errorMessage.classList.add("showElement");
+    }
+
+
     else {
-        errorMSG.style.visibility = "hidden";
-        password.style.borderColor= "#E5E7EB";
-        confirmPassword.style.borderColor = "#E5E7EB";
+        password.classList.add("valid");
+        confirmPassword.classList.add("valid");
     }
 })
